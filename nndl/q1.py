5e9b2093385b890b97961ffd0536b0e64d6e3bb0
BuildTools <unconfigured@null.spigotmc.org>
2025-02-24 22:17:57 -0800

    finish the data preprocess

---
+++ nndl/q1.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+
+def get_X_y(folder_path='dataset/'):
+    df_red = pd.read_csv(folder_path + 'winequality-red.csv').to_numpy()
+    df_white = pd.read_csv(folder_path + 'winequality-white.csv').to_numpy()
+    Red = []
+    White = []
+    for red in df_red:
+        values = red[0].split(';')
+        processed_values = [float(value) if '.' in value else int(value) for value in values]
+        Red.append(processed_values)
+    for white in df_white:
+        values = white[0].split(';')
+        processed_values = [float(value) if '.' in value else int(value) for value in values]
+        White.append(processed_values)
+    Type = np.concatenate([np.zeros(len(Red)), np.ones(len(White))], axis=0).reshape(-1, 1)
+    Red = np.array(Red)
+    White = np.array(White)
+    Wine = np.concatenate([Red, White], axis=0)
+    Wine = np.concatenate([Type, Wine], axis=1)
+    X = Wine[:, :-1]
+    y = Wine[:, -1]
+
+    return X, y
+
+if __name__ == "__main__":
+    X, y = get_X_y()
+    print(X.shape, y.shape)
